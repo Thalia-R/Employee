aae86c8dcf22a3c791925e827714ed54d5bdb76c
messa <thalia.rebecca@gmail.com>
2020-05-29 21:58:08 +0200

    added pagination for the employee form

--- src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    User findByUsername(String username);
-    boolean existsUserByUsername(String username);
-    boolean existsByEmail(String email);
-}
